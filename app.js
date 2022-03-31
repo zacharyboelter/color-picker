946c0e67fef448e92075774892f884265bdc3a87
Zachary Boelter <zboelter@gmail.com>
2022-03-31 14:09:54 -0500

    select all elements

---
+++ app.js
@@ -0,0 +1,8 @@
+
+
+const btn = document.querySelector('button')
+const color = document.querySelectorAll('.color')
+const hex = document.querySelectorAll('.hex')
+
+btn.addEventListener('click', genereateColors)
+
